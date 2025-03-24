313184b0b085c007933bc1a0243438075a5650f7
16BitGlitch <16bitglitch@gmail.com>
2025-03-24 23:25:08 +0000

    Added placeholdrs for gallery

--- pixengine.js
+++ pixengine.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.0.3'; // Match this with meta tag and title
+const APP_VERSION = '1.0.4'; // Match this with meta tag and title
 
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('dragover', (e) => {
